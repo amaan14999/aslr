a7672beda9dcb48b06c821198da573d5f30a8f48
Amaan <73187712+amaan14999@users.noreply.github.com>
2022-05-29 17:22:09 +0530

    Update ObjectDetector.java

--- app/src/main/java/com/amaan/aslr/ObjectDetector.java
+++ app/src/main/java/com/amaan/aslr/ObjectDetector.java
@@ -29,33 +29,35 @@ import java.util.Map;
 import java.util.TreeMap;
 
 public class ObjectDetector {
-    // should start from small letter
-
-    // this is used to load model and predict
-    private final Interpreter interp;
-    //    creating another interpreter for sign language model
-    private final Interpreter interp2;
+    
+    private final Interpreter interp; // we create an Interpreter to encapsulate a pre-trained TensorFlow Lite model, in this case hand_model.tflite
+    private final Interpreter interp2;     // we create another interpreter for sign language model
+    
     // store all label in array
     private final List<String> labelList;
     private final int INPUT_SIZE;
     private final int PIXEL_SIZE = 3; // for RGB
     private final int IMAGE_MEAN = 0;
     private final float IMAGE_STD = 255.0f;
+    
     // use to initialize gpu in app
     private final GpuDelegate graphicsProcessingUnitDelegate;
     private int height = 0;
     private int width = 0;
     private int Classification_Input_Size = 0;
 
+    //                                          hand_model                    input size of hand_model  Sign_lang_model          Input size of Sign_lang_model
     ObjectDetector(AssetManager assetManager, String modelPath, String labelPath, int inputSize, String classification_model, int classification_input_size) throws IOException {
         INPUT_SIZE = inputSize;
         Classification_Input_Size = classification_input_size;
+        
         // use to define gpu or cpu // no. of threads
         Interpreter.Options options = new Interpreter.Options();
         graphicsProcessingUnitDelegate = new GpuDelegate();
         options.addDelegate(graphicsProcessingUnitDelegate);
         options.setNumThreads(4); // set it according to your phone
-        // loading model
+        
+        // loading hand_model
         interp = new Interpreter(loadModelFile(assetManager, modelPath), options);
         // load labelmap
         labelList = loadLabelList(assetManager, labelPath);
@@ -71,11 +73,14 @@ public class ObjectDetector {
     }
 
     private List<String> loadLabelList(AssetManager assetManager, String labelPath) throws IOException {
+        
         // to store label
         List<String> labelList = new ArrayList<>();
+        
         // create a new reader
         BufferedReader reader = new BufferedReader(new InputStreamReader(assetManager.open(labelPath)));
         String line;
+        
         // loop through each line and store it to labelList
         while ((line = reader.readLine()) != null) {
             labelList.add(line);
@@ -97,23 +102,21 @@ public class ObjectDetector {
 
     // create new Mat function
     public Mat recognizeImage(Mat mat_image) {
+        
         // Rotate original image by 90 degree get get portrait frame
-
-        // This change was done in video: Does Your App Keep Crashing? | Watch This Video For Solution.
-        // This will fix crashing problem of the app
-
         Mat rotated_mat_image = new Mat();
-
         Mat a = mat_image.t();
         Core.flip(a, rotated_mat_image, 1);
         // Release mat
         a.release();
-
         // if you do not do this process you will get improper prediction, less no. of object
+        
+        
         // now convert it to bitmap
         Bitmap bitmap = null;
         bitmap = Bitmap.createBitmap(rotated_mat_image.cols(), rotated_mat_image.rows(), Bitmap.Config.ARGB_8888);
         Utils.matToBitmap(rotated_mat_image, bitmap);
+        
         // define height and width
         height = bitmap.getHeight();
         width = bitmap.getWidth();
@@ -123,90 +126,96 @@ public class ObjectDetector {
 
         // convert bitmap to bytebuffer as model input should be in it
         ByteBuffer byteBuffer = convertBitmapToByteBuffer(scaledBitmap);
+        
 
         // defining output
         // 10: top 10 object detected
-        // 4: there coordinate in image
+        // 4: their coordinate in image
         //  float[][][]result=new float[1][10][4];
         Object[] input = new Object[1];
         input[0] = byteBuffer;
 
         Map<Integer, Object> output_map = new TreeMap<>();
-        // we are not going to use this method of output
-        // instead we create treemap of three array (boxes,score,classes)
+        // we create treemap of three array (boxes,score,classes)
 
         float[][][] boxes = new float[1][10][4];
         // 10: top 10 object detected
-        // 4: there coordinate in image
+        // 4: their coordinate in image
+        
+        
         float[][] scores = new float[1][10];
         // stores scores of 10 object
+        
+        
         float[][] classes = new float[1][10];
         // stores class of object
 
+        
         // add it to object_map;
         output_map.put(0, boxes);
         output_map.put(1, classes);
         output_map.put(2, scores);
 
-        // now predict
+        // now we predict
         interp.runForMultipleInputsOutputs(input, output_map);
-        // Before watching this video please watch my previous 2 video of
-        //      1. Loading tensorflow lite model
-        //      2. Predicting object
-        // In this video we will draw boxes and label it with it's name
-
         Object value = output_map.get(0);
         Object Object_class = output_map.get(1);
         Object score = output_map.get(2);
 
+        
         // loop through each object
         // as output has only 10 boxes
         for (int i = 0; i < 10; i++) {
-            //here we are looping through each hand which is detected -Amaan
+            //here we are looping through each hand which is detected
             float class_value = (float) Array.get(Array.get(Object_class, 0), i);
             float score_value = (float) Array.get(Array.get(score, 0), i);
             // define threshold for score
-
+            
             // Here you can change threshold according to your model
-            // Now we will do some change to improve app
             if (score_value > 0.5) {
                 Object box1 = Array.get(Array.get(value, 0), i);
-                // we are multiplying it with Original height and width of frame
-                //change this into x1,y1 and x2,y2 coordinates -Amaan
-
+            
+                
+                // we are multiplying box1 with Original height and width of frame
+                //(x1,y1) is the starting point of the hand
+                //(x2,y2) is the end point of the hand
                 float y1 = (float) Array.get(box1, 0) * height;
                 float x1 = (float) Array.get(box1, 1) * width;
                 float y2 = (float) Array.get(box1, 2) * height;
                 float x2 = (float) Array.get(box1, 3) * width;
 
-                //set boundary limit -Amaan
-                if (y1 < 0) {
+                //setting boundary limit
+                if (y1 < 0) 
+                {
                     y1 = 0;
                 }
-                if (x1 < 0) {
+                if (x1 < 0) 
+                {
                     x1 = 0;
                 }
-                if (x2 > width) {
+                if (x2 > width) 
+                {
                     x2 = width;
                 }
-                if (y2 > height) {
+                if (y2 > height) 
+                {
                     y2 = height;
                 }
-                //now set height and width of box
+                
+                
+                //now we set height and width of box
+                //since, (x1,y1) is the starting point of the hand
+                //(x2,y2) is the end point of the hand, the width and height will be:
                 float w1 = x2 - x1;
                 float h1 = y2 - y1;
 
-                //so if you don't know
-                //(x1,y1) is the starting point of the hand
-                //(x2,y2) is the end point of the hand
 
                 //crop hand image from original frame
-
                 Rect cropped_roi = new Rect((int) x1, (int) y1, (int) w1, (int) h1);
                 Mat cropped = new Mat(rotated_mat_image, cropped_roi).clone();
 
+                
                 //Now convert this cropped Mat to Bitmap
-
                 Bitmap bitmap1 = null;
                 bitmap1 = Bitmap.createBitmap(cropped.cols(), cropped.rows(), Bitmap.Config.ARGB_8888);
                 Utils.matToBitmap(cropped, bitmap1);
@@ -214,43 +223,41 @@ public class ObjectDetector {
 
                 //Resize bitmap to classification input size=96
                 Bitmap scaledBitmap1 = Bitmap.createScaledBitmap(bitmap1, Classification_Input_Size, Classification_Input_Size, false);
+                
                 //convert scaled bitmap to byte buffer
                 ByteBuffer byteBuffer1 = convertBitmapToByteBuffer1(scaledBitmap1);
 
-                //create an array of output of interpreter2
+                //create an array of output of interpr2
                 float[][] output_class_value = new float[1][1];
 
                 //predict output for bytebuffer1
                 interp2.run(byteBuffer1, output_class_value);
 
-                //If you want to see output_class_value
+                //Just to check the output_class_value
                 Log.d("objectDetectionClass", "output_class_value: " + output_class_value[0][0]);
-                //convert output_class_value to alphabets
-                //now create get_alphabets function
-
+                
+                //now we need to convert output_class_value to alphabets
+                //to do that we will create get_alphabets function
                 String sign_value = get_alphabets(output_class_value[0][0]);
+                
                 //use puttext to add Class name in image
-                //              input/output        text            starting point              font size                   text color:white
+                //              input/output      text            starting point                font size              text color:white
                 Imgproc.putText(rotated_mat_image, "" + sign_value, new Point(x1 + 10, y1 + 40), 2, 1.5, new Scalar(255, 255, 255, 255), 2);
 
-                // draw rectangle in Original frame //  starting point    // ending point of box  // color of box       thickness
+                // draw rectangle in Original frame //  starting point    // ending point of box  // color of box   thickness
                 Imgproc.rectangle(rotated_mat_image, new Point(x1, y1), new Point(x2, y2), new Scalar(0, 255, 0, 255), 2);
-                //Now we will change Layout
             }
 
         }
-        // select device and run
 
-        // before returning rotate back by -90 degree
-
-        // Do same here
+        // after prediction, rotate back by -90 degree
         Mat b = rotated_mat_image.t();
         Core.flip(b, mat_image, 0);
         b.release();
-        // Now for second change go to CameraBridgeViewBase
         return mat_image;
     }
-
+    
+    //function get_alphabets, to convert the number received in line 241 and convert it into its corresponding alphabet
     private String get_alphabets(float sig_v) {
         String val = "";
         if (sig_v >= -0.5 & sig_v < 0.5) {
@@ -307,13 +314,10 @@ public class ObjectDetector {
 
         return val;
     }
-
+    
+    //this model is for hand_model
     private ByteBuffer convertBitmapToByteBuffer(Bitmap bitmap) {
         ByteBuffer byteBuffer;
-        // some model input should be quant=0  for some quant=1
-        // for this quant=0
-        // Change quant=1
-        // As we are scaling image from 0-255 to 0-1
         int quant = 1;
         int size_images = INPUT_SIZE;
         if (quant == 0) {
@@ -325,9 +329,6 @@ public class ObjectDetector {
         int[] intValues = new int[size_images * size_images];
         bitmap.getPixels(intValues, 0, bitmap.getWidth(), 0, 0, bitmap.getWidth(), bitmap.getHeight());
         int pixel = 0;
-
-        // some error
-        //now run
         for (int i = 0; i < size_images; ++i) {
             for (int j = 0; j < size_images; ++j) {
                 final int val = intValues[pixel++];
@@ -346,14 +347,14 @@ public class ObjectDetector {
         return byteBuffer;
     }
 
-    //create a copy of createbitmap to byte buffer
-    //add 1
 
+    
+    //we create another bytebuffer for sign language model
     private ByteBuffer convertBitmapToByteBuffer1(Bitmap bitmap) {
         ByteBuffer byteBuffer;
 
         int quant = 1;
-        //change input size
+        //setting input size of the sign language model
         int size_images = Classification_Input_Size;
         if (quant == 0) {
             byteBuffer = ByteBuffer.allocateDirect(1 * size_images * size_images * 3);
@@ -364,7 +365,7 @@ public class ObjectDetector {
         int[] intValues = new int[size_images * size_images];
         bitmap.getPixels(intValues, 0, bitmap.getWidth(), 0, 0, bitmap.getWidth(), bitmap.getHeight());
         int pixel = 0;
-        //remove 255.0f as we did not scale the image
+        
 
         for (int i = 0; i < size_images; ++i) {
             for (int j = 0; j < size_images; ++j) {
@@ -384,5 +385,3 @@ public class ObjectDetector {
     }
 
 }
-// Next video is about drawing box and labeling it
-// If you have any problem please inform me
